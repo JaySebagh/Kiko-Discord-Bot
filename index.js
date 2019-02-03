394ba3123cabecc9a6c3f44820e3c915646ee752
JaySebagh <jaysebagh@gmail.com>
2019-02-02 16:55:41 -0800

    added a .cat image fetcher

--- index.js
+++ index.js
@@ -2,19 +2,28 @@ require('dotenv').config()
 const Discord = require("discord.js");
 const token = process.env.DISCORD_KEY;
 const client = new Discord.Client();
+const superagent = require("superagent");
 
 client.on('ready', () => {
     console.log('Bot is now connected');
     // client.channels.find(x => x.name === "general").send("Kiko has connected!");
 })
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
     if (msg.content === ".hello") {
         msg.channel.send(`Hello ${msg.author}!`);
     }
     if (msg.content === ".master") {
         msg.channel.send(`My creator is Koe`);
     }
-})
+    if (msg.content === ".cat") {
+        let {body} = await superagent.get("http://aws.random.cat/meow")
+        if(!{body}) return msg.channel.send("Error. Try Again.")
+            let cEmbed = new Discord.RichEmbed()
+            .setImage(body.file)
+            msg.channel.send({embed: cEmbed})
+            msg.delete();
+    }
+});
 
 client.login(token);
