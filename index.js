fa9abf63e7fc80278514789f452a0d887d149f7e
JaySebagh <jaysebagh@gmail.com>
2019-02-10 16:40:29 -0800

    added now playing

--- index.js
+++ index.js
@@ -119,4 +119,6 @@ function play(guild, song) {
         })
         .on("error", error => console.error(error));
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+
+    serverQueue.textChannel.send(`Now Playing: **${song.title}**`);
 }
